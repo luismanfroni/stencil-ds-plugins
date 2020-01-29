eb989ae7534c7bb19ce11ec671c8010b380c09cf
Luis Felipe Manfroni <manfroni.luis@gmail.com>
2020-01-29 19:13:17 -0300

    feat(vue): init vue output package

---
+++ packages/vue-output-target/src/plugin.ts
@@ -0,0 +1,39 @@
+import { Config, OutputTargetCustom } from '@stencil/core/internal';
+import { OutputTargetVue } from './types';
+import { normalizePath } from './utils';
+import path from 'path';
+
+export const vueOutputTarget = (outputTarget: OutputTargetVue): OutputTargetCustom => ({
+    type: 'custom',
+    name: 'vue-library',
+    validate(config) {
+      return normalizeOutputTarget(config, outputTarget);
+    },
+    async generator(config, compilerCtx, buildCtx) {
+      const timespan = buildCtx.createTimeSpan(`generate react started`, true);
+  
+      //await reactProxyOutput(compilerCtx, outputTarget, buildCtx.components, config);
+  
+      timespan.finish(`generate react finished`);
+    }
+});
+
+function normalizeOutputTarget(config: Config, outputTarget: any) {
+    const results: OutputTargetVue = {
+      ...outputTarget,
+      excludeComponents: outputTarget.excludeComponents || []
+    };
+  
+    if (config.rootDir == null) {
+      throw new Error('rootDir is not set and it should be set by stencil itself');
+    }
+    if (outputTarget.proxiesFile == null) {
+      throw new Error('proxiesFile is required');
+    }
+  
+    if (outputTarget.directivesProxyFile && !path.isAbsolute(outputTarget.directivesProxyFile)) {
+      results.proxiesFile = normalizePath(path.join(config.rootDir, outputTarget.proxiesFile));
+    }
+  
+    return results;
+}
---
+++ packages/vue-output-target/src/types.ts
@@ -0,0 +1,11 @@
+export interface OutputTargetVue {
+    componentCorePackage?: string;
+    proxiesFile: string;
+    excludeComponents?: string[];
+    loaderDir?: string;
+  }
+  
+  export interface PackageJSON {
+    types: string;
+  }
+  
