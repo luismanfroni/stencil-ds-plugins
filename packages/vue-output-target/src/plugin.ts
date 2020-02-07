fd21ca654a5f5948707496b539a406ac2761b668
Luis Felipe Manfroni <manfroni.luis@gmail.com>
2020-02-07 19:21:46 -0300

    feat(vue): plugin messages and vue compile

--- packages/vue-output-target/src/plugin.ts
+++ packages/vue-output-target/src/plugin.ts
@@ -1,6 +1,7 @@
 import { Config, OutputTargetCustom } from '@stencil/core/internal';
 import { OutputTargetVue } from './types';
 import { normalizePath } from './utils';
+import { vueProxyOutput } from './output-vue';
 import path from 'path';
 
 export const vueOutputTarget = (outputTarget: OutputTargetVue): OutputTargetCustom => ({
@@ -10,11 +11,9 @@ export const vueOutputTarget = (outputTarget: OutputTargetVue): OutputTargetCust
       return normalizeOutputTarget(config, outputTarget);
     },
     async generator(config, compilerCtx, buildCtx) {
-      const timespan = buildCtx.createTimeSpan(`generate react started`, true);
-  
-      //await reactProxyOutput(compilerCtx, outputTarget, buildCtx.components, config);
-  
-      timespan.finish(`generate react finished`);
+      const timespan = buildCtx.createTimeSpan(`generate vue started`, true);
+      await vueProxyOutput(compilerCtx, outputTarget, buildCtx.components);
+      timespan.finish(`generate vue finished`);
     }
 });
 
@@ -23,17 +22,14 @@ function normalizeOutputTarget(config: Config, outputTarget: any) {
       ...outputTarget,
       excludeComponents: outputTarget.excludeComponents || []
     };
-  
     if (config.rootDir == null) {
       throw new Error('rootDir is not set and it should be set by stencil itself');
     }
     if (outputTarget.proxiesFile == null) {
       throw new Error('proxiesFile is required');
     }
-  
     if (outputTarget.directivesProxyFile && !path.isAbsolute(outputTarget.directivesProxyFile)) {
       results.proxiesFile = normalizePath(path.join(config.rootDir, outputTarget.proxiesFile));
     }
-  
     return results;
 }
